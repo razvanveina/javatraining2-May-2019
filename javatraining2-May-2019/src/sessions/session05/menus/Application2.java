72861cfc44147a5e94fd7694f890c50f56517d5d
Razvan <Razvan@192.168.1.33>
2019-06-13 18:48:00 +0300

    Menu framework

--- javatraining2-May-2019/src/sessions/session05/menus/Application2.java
+++ javatraining2-May-2019/src/sessions/session05/menus/Application2.java
@@ -1,17 +1,36 @@
 package sessions.session05.menus;
 
+import sessions.session05.mls.Language;
+
 public class Application2 {
 	public static void main(String[] args) {
 		Menu mainMenu = new Menu("", "");
+
 		Menu changeLanguageMenu = new Menu("1", "/chlang");
-		mainMenu.addMenuItem(changeLanguageMenu);
-		MenuItem exitMenu = new MenuItem("0", "/exit") {
+
+		MenuItem setEnglishMenu = new MenuItem("1", "/en") {
 			@Override
 			public void doAction() {
+				MultilanguageSession.mls.setLanguage(Language.EN);
 			}
 		};
-		mainMenu.setBackAction(exitMenu);
+
+		changeLanguageMenu.addMenuItem(setEnglishMenu);
+
+		MenuItem setRomanianMenu = new MenuItem("2", "/ro") {
+			@Override
+			public void doAction() {
+				MultilanguageSession.mls.setLanguage(Language.RO);
+			}
+		};
+		changeLanguageMenu.addMenuItem(setRomanianMenu);
+		MenuItem backMenu = new BackMenu("0", "/back");
+		changeLanguageMenu.addMenuItem(backMenu);
+		changeLanguageMenu.setBackAction(backMenu);
+		mainMenu.addMenuItem(changeLanguageMenu);
+		MenuItem exitMenu = new BackMenu("0", "/exit");
 		mainMenu.addMenuItem(exitMenu);
+		mainMenu.setBackAction(exitMenu);
 
 		mainMenu.doAction();
 	}
---
+++ javatraining2-May-2019/src/sessions/session05/menus/BackMenu.java
@@ -0,0 +1,14 @@
+package sessions.session05.menus;
+
+public class BackMenu extends MenuItem {
+
+	public BackMenu(String option, String key) {
+		super(option, key);
+	}
+
+	@Override
+	public void doAction() {
+
+	}
+
+}
